91fa137803754c72fad56d02bd88171a6f96a7e5
GrayHead <ss.zhuravlov@gmail.com>
2024-10-07 20:12:40 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Xxx from "./componets/Xxx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Xxx place={'house'}/>
+            <Xxx place={'club'}/>
+
+
+        </div>
+
+    );
 }
 
 export default App;
