f4320a4d5d4411b183be72da4a51b407bd3ab127
GrayHead <ss.zhuravlov@gmail.com>
2024-10-21 20:21:04 +0300

    form joi

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {userValidator} from "../validators/user.validator";
 
 interface FormProps {
     username: string;
@@ -17,7 +19,7 @@ const FormComponent = () => {
             isValid
         }
 
-    } = useForm<FormProps>({mode: 'all'});
+    } = useForm<FormProps>({mode: 'all', resolver: joiResolver(userValidator)});
 
     const customHandler = (dataFromForm: FormProps) => {
         console.log(dataFromForm);
@@ -29,26 +31,18 @@ const FormComponent = () => {
             <div>
                 <label>
 
-                    <input type="text" placeholder={'username'} {...register('username', {
-                        required: {value: true, message: "username is required"},
-                    })}/>
+                    <input type="text" placeholder={'username'} {...register}/>
                     {errors.username && <div>{errors.username.message}</div>}
                 </label>
             </div>
             <div>
-                <label><input type="text" placeholder={'password'} {...register('password', {
-                    minLength: {value: 3, message: 'too short'},
-                    maxLength: {value: 6, message: 'too long'},
-                })}/>
+                <label><input type="text" placeholder={'password'} {...register('password')}/>
                     {errors.password && <div>{errors.password.message}</div>}
                 </label>
             </div>
             <div>
                 <label>
-                    <input type="number" placeholder={'age'} {...register('age', {
-                        min: {value: 0, message: 'too young'},
-                        max: {value: 121, message: 'too old'},
-                    })}/>
+                    <input type="number" placeholder={'age'} {...register('age')}/>
                     {errors.age && <div>{errors.age.message}</div>}
                 </label>
             </div>
