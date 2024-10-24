9c31048310de26755ae67c86f96ea7911c25fca7
GrayHead <ss.zhuravlov@gmail.com>
2024-10-24 19:34:19 +0300

    cons 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import obj3, {obj1, obj2} from "./asd";
 
 
-function App() {
+export function App() {
     return (
         <div>
+            {
+                JSON.stringify(obj1)
+            }
+            {
+                JSON.stringify(obj2)
+            }
 
+            {JSON.stringify(obj3)}
 
         </div>
 
---
+++ src/asd.ts
@@ -0,0 +1,11 @@
+/*export*/
+const obj1 = {}
+export const obj2 = {}
+
+
+const obj3 = {}
+export {
+    obj1
+}
+
+export default obj3;
