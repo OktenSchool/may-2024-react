f108d2f60c46d29ee3c1cdaea49df27677cce256
GrayHead <ss.zhuravlov@gmail.com>
2024-10-28 20:29:06 +0200

    useStet useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 
 function App() {
+
+    let [number, setNumber] = useState<number>(0);
+
+
+    useEffect(() => {
+        console.log('connect to server, send to server', number);
+        return () => {
+            console.log('unconnect from server');
+        }
+    }, [number]);
+
+
     return (
         <div>
 
+            <h2>{number}</h2>
+
+            <button onClick={() => {
+                setNumber(prevState => prevState + 1);
+            }}>increment
+            </button>
+            <button onClick={() => {
+                setNumber(prevState => {
+                    return --prevState;
+                });
+
+            }}>decrement
+            </button>
 
         </div>
 
